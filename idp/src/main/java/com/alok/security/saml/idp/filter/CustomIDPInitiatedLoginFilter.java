294c96e015010a41b39e72b959f5eda3c0113dd6
alokkusingh <alok.ku.singh@gmail.com>
2020-07-29 13:12:11 +0530

    fixed smal attribute issue: https://github.com/alokkusingh/saml/issues/1

--- idp/src/main/java/com/alok/security/saml/idp/filter/CustomIDPInitiatedLoginFilter.java
+++ idp/src/main/java/com/alok/security/saml/idp/filter/CustomIDPInitiatedLoginFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.saml.SamlRequestMatcher;
 import org.springframework.security.saml.provider.identity.IdentityProviderService;
 import org.springframework.security.saml.provider.identity.IdpInitiatedLoginFilter;
 import org.springframework.security.saml.provider.provisioning.SamlProviderProvisioning;
+import org.springframework.security.saml.saml2.authentication.AuthenticationRequest;
 import org.springframework.security.saml.saml2.metadata.NameId;
 import org.springframework.security.saml.saml2.metadata.ServiceProviderMetadata;
 
@@ -27,7 +28,10 @@ public class CustomIDPInitiatedLoginFilter extends IdpInitiatedLoginFilter {
         super(provisioning, assertionStore, requestMatcher);
     }
 
+
+    @Override
     protected Assertion getAssertion(Authentication authentication,
+                                     AuthenticationRequest authenticationRequest,
                                      IdentityProviderService provider,
                                      ServiceProviderMetadata recipient) {
         Assertion assertion = provider.assertion(recipient, authentication.getName(), NameId.PERSISTENT);
